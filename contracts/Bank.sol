52dc3595d3d4e0cca5eb86aec58495fcb5d11594
tomlion <qycpublic@gmail.com>
2016-01-09 00:35:56 +0800

    Add Draft transfer

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -7,7 +7,9 @@ contract Bank is Entity{
     name = _name;
   }
 
-  function createDraft(address _payer, address _payee, uint _amount, uint _rate, uint _mature) {
-    drafts.create(address(this), _payer, _payee, _amount, _rate, _mature);
+  function createDraft(address _payer, address _payee, uint _amount, uint _rate, uint _mature) returns (address) {
+    address latest = drafts.create(address(this), _payer, _payee, _amount, _rate, _mature);
+    //Draft(latest).transfer(address(this), _payer, 'Company');
+    return latest;
   }
 }
