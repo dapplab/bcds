3c30f52923593ee37b6aee794f2c9caa3990944e
Bin He <beenhero@gmail.com>
2016-01-09 17:17:27 +0800

    wire up form onsubmit callback.

--- app/src/component/CreateDraft.jsx
+++ app/src/component/CreateDraft.jsx
@@ -1,22 +1,42 @@
 import React from 'react';
-import {Form, Input, Select, Checkbox, Radio, DatePicker} from 'antd';
+import {Form, Input, Select, Checkbox, Radio, DatePicker, Row, Col, Button} from 'antd';
 
 const FormItem = Form.Item;
 const Option = Select.Option;
 const RadioGroup = Radio.Group;
 import BankStore from "../stores/BankStore" ;
 
-export default class CreateDraft extends React.Component {
+const CreateDraft = React.createClass({
+  mixins: [Form.ValueMixin],
+  
+  getInitialState() {
+    return {
+      formData: {
+        draftPayee: 'me'
+      }
+    };
+  },
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('收到表单值~~~ ：' + JSON.stringify(this.state.formData, function(k, v) {
+      if (typeof v === 'undefined') {
+        return '';
+      }
+      return v;
+    }));
+  },
 
   render() {
+    const formData = this.state.formData;
     return (
-      <div className="form-general">
-        <Form horizontal>
+      <div className="main-container-center form-general">
+        <Form horizontal onSubmit={this.handleSubmit}>
           <FormItem
             label="收款人："
             labelCol={{span: 6}}
             wrapperCol={{span: 14}}>
-            <Input id="draft-payee" placeholder="Please enter..." />
+            <Input id="draft-payee" name="draftPayee" placeholder="Please enter..." />
           </FormItem>
           <FormItem
             label="出票金额："
@@ -59,9 +79,17 @@ export default class CreateDraft extends React.Component {
             <Input type="textarea" id="draft-memo" rows="3" />
           </FormItem>
 
+          <Row>
+            <Col span="16" offset="6">
+              <Button type="primary" htmlType="submit">Submit</Button>
+            </Col>
+          </Row>
+
         </Form>
       </div>
     );
   }
 
-}
+});
+
+export default CreateDraft;
