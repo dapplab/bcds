681bb07496fe387ddb376f0143afd620c1c7a524
tomlion <qycpublic@gmail.com>
2016-01-09 11:08:13 +0800

    Add genesis.json and accounts for test

--- contracts/Company.sol
+++ contracts/Company.sol
@@ -1,7 +1,12 @@
 contract Company {
   bytes32 public name;
+  address[] drafts;
 
   function setName(bytes32 _name) {
     name = _name;
+  }
+
+  function addDraft(address _draft) {
+    drafts.push(_draft);
   }
 }
