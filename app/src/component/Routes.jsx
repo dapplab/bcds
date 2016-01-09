3ddcef99e6540f35a4d6d0599cadd852f5aa202e
Bin He <beenhero@gmail.com>
2016-01-09 08:38:24 +0800

    Add Company's drafts list page.

--- app/src/component/Routes.jsx
+++ app/src/component/Routes.jsx
@@ -4,6 +4,7 @@ import Home from './Home';
 import Bank from './Bank';
 import Company from './Company';
 import CreateDraft from './CreateDraft';
+import CompanyDrafts from './CompanyDrafts';
 
 export default class Routes extends React.Component {
   render() {
@@ -11,7 +12,9 @@ export default class Routes extends React.Component {
       <Route path="/">
         <IndexRoute component={Home} />
         <Route  path="bank" component={Bank}/>
-        <Route  path="company" component={Company}/>
+        <Route  path="company">
+          <Route  path="/company/drafts" component={CompanyDrafts}/>  
+        </Route>
         <Route  path="draft/new" component={CreateDraft}/>
       </Route>
     </Router>) ;
