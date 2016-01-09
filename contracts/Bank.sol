c3c21bc129ba740a261e9c80e4a6075f3557de61
tomlion <qycpublic@gmail.com>
2016-01-09 13:23:43 +0800

    Event for creating Bank

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -4,8 +4,16 @@ contract Bank{
   bytes32 public name;
   address[] drafts;
 
+  event CreatedBank(bytes32 indexed topic, bytes32 name);
+
+  function Bank(bytes32 _name){
+    name = _name;
+    CreatedBank('BANK', _name);
+  }
+
   function setName(bytes32 _name) {
     name = _name;
+    CreatedBank('BANK', _name);
   }
 
   function createDraft(address _payer, address _payee, uint _amount, uint _rate, uint _mature) returns (address) {
