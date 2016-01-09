e5f4d44c2764a25c215d7d8171d0bb2f5ba82eb3
u2 <zhangyaning1985@gmail.com>
2016-01-09 10:06:02 +0800

    keep drafts record in entity

--- contracts/Entity.sol
+++ contracts/Entity.sol
@@ -4,6 +4,20 @@ contract Entity {
   address public owner;
   address[] drafts;
 
+  function addDraft(address _address) returns (bool){
+    drafts.push(_address);
+  }
+
+  function removeDraft(address _address) returns (bool){
+    for(uint i = 0; i < drafts.length; i++){
+      if(drafts[i] == _address){
+        delete drafts[i];
+        return true;
+      }
+    }
+    return false;
+  }
+
   modifier onlyowner() {
     if (msg.sender != owner)
       throw;
