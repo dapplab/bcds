e5268542d9f893872c1df1e489733076b907112d
u2 <zhangyaning1985@gmail.com>
2016-01-09 08:21:50 +0800

    only bank owner can create draft

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -5,7 +5,7 @@ contract Bank is Entity{
     name = _name;
   }
 
-  function createDraft(address _payer, address _payee, uint _amount, uint _rate, uint _mature) returns (address) {
+  function createDraft(address _payer, address _payee, uint _amount, uint _rate, uint _mature) onlyowner returns (address) {
     address latest = new Draft(msg.sender, _payer, _payee, _amount, _rate, _mature);
     drafts.push(latest);
     //Draft(latest).transfer(address(this), _payer, 'Company');
--- contracts/Entity.sol
+++ contracts/Entity.sol
@@ -2,5 +2,12 @@ import 'Draft';
 
 contract Entity {
   bytes32 public name;
+  address public owner;
   address[] drafts;
+
+  modifier onlyowner() {
+    if (msg.sender != owner)
+      throw;
+    _
+  }
 }
