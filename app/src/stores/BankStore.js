22c18abb28f0d354c4a65777e450299e500454d1
u2 <zhangyaning1985@gmail.com>
2016-01-09 09:07:14 +0800

    web3 setupBankContract demo

--- app/src/stores/BankStore.js
+++ app/src/stores/BankStore.js
@@ -1,4 +1,15 @@
 export default class BankStore {
+  constructor(props) {
+    this.bankContract = setupBankContract;
+  }
+
+  setupBankContract() {
+    var BankWeb3 = web3.eth.contract(Bank.abi);
+    var bankContract = BankWeb3.at(Bank.address);
+    console.log('bankContract init..');
+    return bankContract;
+  }
+
   static banks = [
     {bankName: "中国银行", addr: "1a"},
     {bankName: "中国工商银行", addr: "1b"},
